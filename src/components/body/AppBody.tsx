7d5caee3f6c7b9e166ac7b36f55093e6f8aa27cd
Toni Margan <Toni.Margan@infobip.com>
2021-07-23 15:03:45 +0200

    installed axios, and implemented simple api request

--- src/components/body/AppBody.tsx
+++ src/components/body/AppBody.tsx
@@ -1,12 +1,21 @@
+import axios from 'axios';
 import React from 'react';
 
 import { RequestBuilder, ResponsePreview, UrlForm } from '../';
 import './AppBody.css';
 
 export const AppBody: React.FC = () => {
+  const onSendRequest = async (url: string, method: string) => {
+    const response = await axios({
+      method: 'get',
+      url: url,
+    });
+    console.log(response);
+  };
+
   return (
     <div className='app-body'>
-        <UrlForm />
+        <UrlForm onSendRequest={onSendRequest} />
         <div className='app-body__request'>
             <RequestBuilder />
             <ResponsePreview />
--- src/components/url-form/UrlForm.tsx
+++ src/components/url-form/UrlForm.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { MethodPicker } from './MethodPicker';
 import './UrlForm.css';
 
-export const UrlForm: React.FC = () => {
+interface Props {
+  onSendRequest: (url: string, method: string) => void;
+}
+
+export const UrlForm: React.FC<Props> = (props: Props) => {
+  const [method, setMethod] = useState<string>('GET');
+  const [url, setUrl] = useState<string>('');
+
+  const onUrlChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setUrl(e.currentTarget.value);
+  };
+
+  const onSendRequest = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    props.onSendRequest(url, method);
+  };
+
   return (
-    <form className='url-form'>
+    <form className='url-form' onSubmit={onSendRequest}>
         <MethodPicker />
-        <input className="url-form__input" type='text' />
-        <input className="url-form__submit" type='submit' value='Send' />
+        <input
+          className="url-form__input"
+          type='text'
+          value={url}
+          onChange={onUrlChange}
+        />
+        <input
+          className="url-form__submit"
+          type='submit'
+          value='Send'
+        />
     </form>
   );
 }
