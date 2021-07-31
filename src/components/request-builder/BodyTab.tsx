082351d81ec57d17d8583f077e7b49e9cc69b0df
Toni Margan <toni.margan@gmail.com>
2021-07-31 16:53:35 +0200

    created boilerplate components for request builder

---
+++ src/components/request-builder/BodyTab.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './BodyTab.css';
+
+interface Props {
+}
+
+export const BodyTab: React.FC<Props> = (props: Props) => {  
+  return (
+    <div className="request-builder-body"> 
+      body
+    </div>
+  );
+}
--- src/components/request-builder/RequestBuilder.tsx
+++ src/components/request-builder/RequestBuilder.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BodyTab } from './BodyTab';
 
 import './RequestBuilder.css';
 
+enum RequestTab {
+  BODY,
+  QUERY_PARAMS,
+  HEADERS,
+}
+
 export const RequestBuilder: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<RequestTab>(RequestTab.BODY);
+
+  const renderRequestBuilder = () => {  
+    switch (activeTab) {
+      case RequestTab.BODY:
+        return <BodyTab />
+
+      case RequestTab.QUERY_PARAMS:
+        return <div>PARAMS</div>
+      
+      case RequestTab.HEADERS:
+        return <div>headers</div>
+    }
+  };
+  
   return (
-    <div className='request-builder'>request builder</div>
+    <div className="request-builder">
+      <div className="request-builder-title">
+        <span>Request</span>
+      </div>
+      <div className="request-builder-tabs">
+        <button className="request-builder-tabs__button" onClick={() => setActiveTab(RequestTab.BODY)}>Body</button>
+        <button className="response-preview-tabs__button" onClick={() => setActiveTab(RequestTab.QUERY_PARAMS)}> Query params</button>
+        <button className="response-preview-tabs__button" onClick={() => setActiveTab(RequestTab.HEADERS)}> Headers</button>
+      </div>
+      {renderRequestBuilder()}
+    </div>
   );
 }
